c9d4c4efda9b434e456b990ff92635911b2fdc4b
belindaam7 <bsamezquita@gmail.com>
2024-08-10 23:00:42 -0700

    Created react app and added bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from dictionary app</h1>
+      <a href="#" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
+}
+
+export default App;
