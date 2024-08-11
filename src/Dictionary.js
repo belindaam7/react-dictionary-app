6a7d76fc6dfa4f37a5db6a512c0973872bc81293
belindaam7 <bsamezquita@gmail.com>
2024-08-11 00:07:19 -0700

    Integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
+
+  function handleResponse(response) {
+    console.log(response.data.meanings[0].definition);
+  }
+
+  let apiKey = "bec049cdcofb5t08d94f2fc0c3440fa3";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
 
   function search(event) {
     event.preventDefault();
