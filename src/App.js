5fd9819bb67b748ce932d25c500ce126d18cf197
belindaam7 <bsamezquita@gmail.com>
2024-08-10 23:03:48 -0700

    fixed build bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello from dictionary app</h1>
-      <a href="#" className="btn btn-primary">
+      <a href="/" className="btn btn-primary">
         Hello
       </a>
     </div>
